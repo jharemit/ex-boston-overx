ca5661033a66b58731cf51a297790163a099468f
jharemit <jbhare@mit.edu>
2017-02-22 10:52:24 -0500

    firsttry

---
+++ jharemit/exercise.js
@@ -0,0 +1,32 @@
+var exercise = {};
+
+exercise.salaries = null;
+exercise.salary = null;
+
+// load salary and data
+exercise.load = function(salary, boston){
+    exercise.salaries = boston.data;
+    exercise.salary = salary;
+};
+
+// get salaries larger than given salary
+exercise.findBiggerSalaries = function(){
+// CODE FROM LECTURE!
+
+exercise.salaries.filter(exercise.largerSalary);
+
+};
+
+// filter function
+exercise.largerSalary = function largerSalary(item){
+
+    // ---------------------------------------------------
+    //   YOUR CODE
+    //
+    //     Return a boolean (true/false) if item
+    //     is larger than exercise.salary
+    // ---------------------------------------------------
+
+return item[18]>exercise.salary;
+
+};
